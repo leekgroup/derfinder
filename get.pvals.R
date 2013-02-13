7cb7e05c127878497f270bda8c8614c01cdfe668
Alyssa Frazee <acfrazee@gmail.com>
2013-02-13 10:48:59 -0500

    updated p-value calculation

--- get.pvals.R
+++ get.pvals.R
@@ -34,7 +34,7 @@ get.pvals = function(regions, dbfile, tablename, num.perms = 1, group, est.param
 	
 	pvals = rep(NA,dim(regions)[1])
 	for(k in which(regions$state==3|regions$state==4)){
-		pvals[k] = sum(abs(nullstats)>abs(regions$mean.t[k]))/length(nullstats)
+		pvals[k] = (sum(abs(nullstats)>abs(regions$mean.t[k]))+1)/length(nullstats)
 	}
 
 	return(pvals)
