f7fb64e4014869d09db2ac8ce248add40545068a
Alyssa Frazee <acfrazee@gmail.com>
2014-03-04 17:16:27 -0500

    updates to makeDb (column names now work better)

--- R/makeDb.R
+++ R/makeDb.R
@@ -23,20 +23,17 @@
 #'@export
 
 makeDb <- function(dbfile, textfile, tablename, sep = "\t", cutoff = 5){
-	cat(file=dbfile) #create empty file
-	column.names = as.character(as.matrix(read.table(textfile,sep=sep,nrows=1,header=F))) #get column names
-	#print(column.names)
-	# create the sql statement to be used in creating database:
-	for(i in 2:length(column.names)){ 
-	  if(i==2) where.statement = paste(gsub("\\.","_",column.names[i]),">",cutoff,"OR")
-	  else if(i==length(column.names)) where.statement = paste(where.statement,
-	  	gsub("\\.","_",column.names[i]),">",cutoff)
-  	  else where.statement = paste(where.statement,gsub("\\.","_",column.names[i]),">",cutoff,"OR")
-  	  } #(note that "." is not an acceptable character in sql column names - is automatically replaced with "_" - so we do the same in our sql statement)
-	tablename.statement = paste("main",tablename,sep=".")
-	sql.statement = paste("create table",tablename.statement,"as select * from file where",where.statement)
-	#print(where.statement)
-	#print(sql.statement)
-	read.csv.sql(textfile, sql=sql.statement, dbname=dbfile,sep=sep) #this is where all the action is - this creates the database, this is the function in our modified sqldf script.
-	print(paste("Wrote database file",dbfile,"containing table",tablename)) # so that users know this function did something and created a file in their system.
+    cat(file=dbfile) #create empty file
+    column.names = names(read.table(textfile, sep=sep, nrows=1, header=TRUE)) #get column names
+    # create the sql statement to be used in creating database:
+    for(i in 2:length(column.names)){
+        if(i==2) where.statement = paste(gsub("\\.","_",column.names[i]),">",cutoff,"OR")
+        else if(i==length(column.names)) where.statement = paste(where.statement,
+            gsub("\\.","_",column.names[i]),">",cutoff)
+        else where.statement = paste(where.statement,gsub("\\.","_",column.names[i]),">",cutoff,"OR")
+    } #(note that "." is not an acceptable character in sql column names - is automatically replaced with "_" - so we do the same in our sql statement)
+    tablename.statement = paste("main",tablename,sep=".")
+    sql.statement = paste("create table",tablename.statement,"as select * from file where",where.statement)
+    read.csv.sql(textfile, sql=sql.statement, dbname=dbfile,sep=sep) #this is where all the action is - this creates the database, this is the function in our modified sqldf script.
+    print(paste("Wrote database file",dbfile,"containing table",tablename)) # so that users know this function did something and created a file in their system.
 }
